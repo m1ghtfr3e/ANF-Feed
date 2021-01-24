337517a0ec7029ab987fa2fd82cfbff3d3b97e28
m1ghtfr3e <lacreaturesceptique@riseup.net>
2021-01-24 23:00:29 +0100

    Adding Tests

--- tests/test_anffeed.py
+++ tests/test_anffeed.py
@@ -4,6 +4,27 @@ from anfrss import anffeed
 FEED = anffeed.ANFFeed()
 
 
-class TestAnfFeed(unittest.TestCase):
-    def test_set_language(self):
-        pass
+class TestAnfFeedSetLanguage(unittest.TestCase):
+
+    feed = anffeed.ANFFeed()
+
+    def test_english(self):
+        self.feed.set_language('english')
+        self.assertEqual(self.feed.source, 'https://anfenglishmobile.com/feed.rss')
+
+    def test_german(self):
+        # Set Language
+        self.feed.set_language('german')
+        self.assertEqual(self.feed.source, 'https://anfdeutsch.com/feed.rss')
+
+    def test_krumanji(self):
+        self.feed.set_language('kurmanjî')
+        self.assertEqual(self.feed.source, 'https://anfkurdi.com/feed.rss')
+
+    def test_spanish(self):
+        self.feed.set_language('spanish')
+        self.assertEqual(self.feed.source, 'https://anfespanol.com/feed.rss')
+
+    def test_arab(self):
+        self.feed.set_language('arab')
+        self.assertEqual(self.feed.source, 'https://anfarabic.com/feed.rss')
