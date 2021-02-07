0c1d9a1e26433594dc2878de5b4893f65f6abee8
m1ghtfr3e <lacreaturesceptique@riseup.net>
2021-02-07 10:30:56 +0100

    Added repr()

--- anfrss/parser/anffeed.py
+++ anfrss/parser/anffeed.py
@@ -166,6 +166,9 @@ class ANFFeed:
 
         raise NotImplementedError()
 
+    def __repr__(self):
+        return f'{self.__class__.__name__}, {self.source!r}'
+
 
 if __name__ == '__main__':
     pass
